9b351cf46f505e6a1e293be7d8096c0385fe3afd
Helio Brito <helioh3@gmail.com>
2024-03-11 06:53:51 -0300

    feat: install typescript

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multi = void 0;
+function multi(a, b) {
+    return a * b;
+}
+exports.multi = multi;
+console.log(multi(10, 2));
---
+++ main.ts
@@ -0,0 +1,8 @@
+function multi(a: number, b: number): number
+{
+  return a * b;
+}
+
+console.log(multi(10, 2))
+
+export { multi }
